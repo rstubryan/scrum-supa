be7f9ea83f34f07761b50facdead23a1eed7a3c0
rstubryan <restubumirr@gmail.com>
2025-10-21 23:02:13 +0700

    feat: add AuthButton component for user authentication and session handling

---
+++ src/lib/components/auth/AuthButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import UserMenu from './UserMenu.svelte'
+	import { Button } from '$lib/components/ui'
+
+	let { user, session } = $props()
+</script>
+
+{#if user}
+	<UserMenu {user} {session} />
+{:else}
+	<div class="flex items-center space-x-4">
+		<a href="/auth/login">
+			<Button class="border border-gray-300 bg-white text-gray-700 hover:bg-gray-50">Sign in</Button>
+		</a>
+		<a href="/auth/login">
+			<Button>Get started</Button>
+		</a>
+	</div>
+{/if}
