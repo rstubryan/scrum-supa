12b3b9adf36d4e06c9e7fdcd3a3be342738cd34f
rstubryan <restubumirr@gmail.com>
2025-10-22 20:19:25 +0700

    refactor: update import paths for dashboard page

--- src/routes/(protected)/dashboard/+page.svelte
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { getDiscordUserInfo } from '$lib/utils/discord';
+	import { Button } from '@/components/ui/button';
+	import { getDiscordUserInfo } from '@/utils/discord';
 
 	let { data } = $props();
 
